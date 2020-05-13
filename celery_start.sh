76f0ecbbfa0423bf278f0bd0f0237ac8de727213
gumengkai <gumengkai@hotmail.com>
2020-05-13 16:17:04 +0800

    1. 更新celery启动脚本

--- celery_start.sh
+++ celery_start.sh
@@ -1,2 +1,2 @@
 celery multi start w1 -A db_monitor -l info --logfile=logs/celery-worker.log --pidfile=celery-worker.pid
-celery -A monitor beat -l info >  logs/celery-beat.log  2>&1  &
+celery -A db_monitor beat -l info >  logs/celery-beat.log  2>&1  &
