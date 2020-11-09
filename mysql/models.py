86439664d599cdf111f9790d9ff172ef9d1023b7
gumengkai <gumengkai@hotmail.com>
2020-11-09 19:01:05 +0800

    mysql_slowquery host字段调整为256位

--- mysql/models.py
+++ mysql/models.py
@@ -82,7 +82,7 @@ class MysqlStat(models.Model):
 
 
 class MysqlSlowquery(models.Model):
-    tags = models.CharField("标签",max_length=32)
+    tags = models.CharField("标签",max_length=256)
     host = models.CharField("主机ip",max_length=32)
     start_time  = models.CharField("执行时间",max_length=64)
     client_host = models.CharField("主机信息",max_length=64)
