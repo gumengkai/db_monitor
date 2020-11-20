5f8b0b3c54263a107354d7fdee3285173d12c8ac
haochenxiao666 <haochx@yonyou.com>
2020-11-20 16:18:55 +0800

    Update celery_start.sh
    解决环境变量导致celery执行失败

--- celery_start.sh
+++ celery_start.sh
@@ -1,3 +1,4 @@
 rm -rf *.pid
+export PYTHONOPTIMIZE=1
 celery multi start w1 -A db_monitor -l info --logfile=logs/celery-worker.log --pidfile=celery-worker.pid
 celery -A db_monitor beat -l info >  logs/celery-beat.log  2>&1  &
