f7018413badd62c9074f845be23db3993111b516
gumengkai <gumengkai@hotmail.com>
2021-02-18 08:54:10 +0800

    sh脚本格式修改

--- web_start.sh
+++ web_start.sh
@@ -1,3 +1,3 @@
 python manage.py makemigrations &&
 python manage.py migrate &&
-python manage.py runserver 0.0.0.0:8001 > logs/django-web.log 2>&1 &
+python manage.py runserver 0.0.0.0:8000 > logs/django-web.log 2>&1 &
